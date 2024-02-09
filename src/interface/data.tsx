e39d5478a008754ec87a18512240d1cf64bb47bf
Shukur939 <shukursurkhayli000@gmail.com>
2024-02-09 23:41:43 +0400

    basketType

--- src/interface/data.tsx
+++ src/interface/data.tsx
@@ -1,22 +1,25 @@
-export type singleProductsType={
+export type singleProductsType = {
     id: number,
-        title: string,
-        price: string,
-        description: string,
-        category: string,
-        image: string
-        rating: {
-            rate: number,
-            count: number
-        }
+    title: string,
+    price: string,
+    description: string,
+    category: string,
+    image: string
+    rating: {
+        rate: number,
+        count: number
+    }
 }
 
 export interface productTypes {
     products: singleProductsType[]
 }
 export interface basketType {
-    
+    amount: number,
+    totalAmount: number,
+    totalPrice: number,
+    totalDiscountPrice: number,
 }
-export interface selectNameProps{
-    selectName: (value:string) => void
+export interface selectNameProps {
+    selectName: (value: string) => void
 }
